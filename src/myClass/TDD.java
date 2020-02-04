36e9f5a809d796462d1a5abf1b92988e97a75f4a
Md Al Maruf <100641313@oncampus.local>
2020-02-04 13:22:00 -0500

    Test coverage

--- src/myClass/TDD.java
+++ src/myClass/TDD.java
@@ -3,22 +3,26 @@ package myClass;
 public class TDD {
 
 	public String swap(String str) {
+//		AB =>BA
+//		ABCD => ABDC
 		
-		char first = str.charAt(0);		
-		char last = str.charAt(1);
-		return "" + last+first;
+//		char first = str.charAt(0);		
+//		char last = str.charAt(1);
+//		return "" + last+first;
 		
 		
-//		int length = str.length();
-//		if(length<2) {
-//			return str;
-//		}
-//		String subString = str.substring(0,length-2);
-//		char first = str.charAt(0);
-//		char secondlast = str.charAt(length-2);
-//		char last = str.charAt(length-1);
-//		return subString + last + secondlast;
+		int length = str.length();
+		if(length<2) {
+			return str;
+		}
 //		
+		char secondlast = str.charAt(length-2);
+		char last = str.charAt(length-1);
+//		
+		String subString = str.substring(0,length-2);
+//		
+		return subString + last + secondlast;
+//	
 		
 		
 	}
---
+++ src/myClass/TestCoverage.java
@@ -0,0 +1,60 @@
+package myClass;
+
+import java.util.StringTokenizer;
+
+public class TestCoverage {
+	// method that returns maximum number
+	public static int findMax(int arr[]) {
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] < 0) {
+				if (max == 0 || arr[i] > max) {
+					max = arr[i];
+				}
+			} else {
+				if (max < arr[i])
+					max = arr[i];
+			}
+
+		}
+		return max;
+	}
+
+	// method that returns cube of the given number
+	public static int cube(int n) {
+		return n * n * n;
+	}
+
+	// method that returns reverse words
+	public static String reverseWord(String str) {
+
+		StringBuilder result = new StringBuilder();
+
+		// print reversed String
+
+		StringTokenizer tokenizer = new StringTokenizer(str, " ");
+
+		while (tokenizer.hasMoreTokens()) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(tokenizer.nextToken());
+			sb.reverse();
+
+			result.append(sb);
+			result.append(" ");
+		}
+		
+		return result.toString().trim();
+	}
+
+	public static void main(String[] args) {
+		int y[] = { -12, -3, -4, -2 };
+
+		int x = findMax(y);
+		System.out.println(x);
+
+		String st = reverseWord("Hello world");
+		System.out.println(st);
+	}
+
+}
